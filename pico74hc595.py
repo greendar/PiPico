b20f33dd9d783635a461d477ef5171cc0a6a1b6f
Mr Green <dwgreen@hwdsb.on.ca>
2021-11-21 11:22:04 -0500

    Update pico74hc595.py
    updated to take in bits as bits and not as a string based on comments on website

--- pico74hc595.py
+++ pico74hc595.py
@@ -13,16 +13,16 @@ latchPIN=Pin(latchPIN, Pin.OUT)
 clockPIN=Pin(clockPIN, Pin.OUT)
 
 #define shift register update function
-def shift_update(data,data,clock,latch):
+def shift_update(data_bits,data,clock,latch):
   #put latch down to start data sending
   clock.value(0)
   latch.value(0)
-  clock.value(1)
+  #clock.value(1)
   
   #load data in reverse order
   for i in range(7, -1, -1):
     clock.value(0)
-    data.value(int(data[i]))
+    data.value(data_bits &(1<<i))
     clock.value(1)
 
 #put latch up to store data on register
@@ -31,9 +31,9 @@ def shift_update(data,data,clock,latch):
   clock.value(1)
 
 #main program, calling shift register function
-bit_string="00000000"
+
 
 while True:
-    shift_update(bit_string,dataPIN,clockPIN,latchPIN)
-    bit_string = str(random.randint(0, 1))+bit_string[:-1]
+    bit_pattern = random.randint(0, 255)
+    shift_update(bit_pattern,dataPIN,clockPIN,latchPIN)
     utime.sleep(0.3)
